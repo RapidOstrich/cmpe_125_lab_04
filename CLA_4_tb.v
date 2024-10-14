1d80eb843a3b4ee01a31c88cdf70bd8e441cabe5
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2024-10-13 23:13:35 -0700

    Update CLA_4_tb.v

--- CLA_4_tb.v
+++ CLA_4_tb.v
@@ -5,14 +5,14 @@ module CLA_4_tb();
     reg [3:0] a, b;
     reg cin;
     wire [3:0] sum;
-    wire cout, g_g, g_p;
+    wire cout, g_g, p_g;
 
-    CLA_4 instance_0(a, b, cin, sum, cout, g_g, g_p);
+    CLA_4 instance_0(a, b, cin, sum, cout, g_g, p_g);
     
     initial begin
         for (cin = 1'b0; cin < 1'b1; cin = cin + 1)
-            for (b = 1'b0; b < 1'b1; b = b + 1)
-                for (a = 1'b0; a < 1'b1; a = a + 1)
+            for (b = 4'b0000; b < 4'b1111; b = b + 1)
+                for (a = 4'b0000; a < 4'b1111; a = a + 1)
                     #1;
     end
 
